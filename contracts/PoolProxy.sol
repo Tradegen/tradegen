1525b3cfda070209baddc002bbbee25698c3d0c9
Xavier Negron <xaviernegron888@gmail.com>
2021-06-13 18:29:20 -0500

    Finished PoolProxy contract

--- contracts/PoolProxy.sol
+++ contracts/PoolProxy.sol
@@ -1,29 +1,40 @@
 pragma solidity >=0.5.0;
 
-import './libraries/SafeMath.sol';
-
+//Interfaces
 import './interfaces/IPool.sol';
+import './interfaces/ISettings.sol';
 
 import './PoolManager.sol';
-import './Settings.sol';
 
 contract PoolProxy is PoolManager {
     using SafeMath for uint;
 
-    constructor() public {
-        _setPoolProxyAddress(address(this));
+    ISettings public immutable SETTINGS;
+
+    constructor(ISettings settings, IUserPoolFarm farm, IAddressResolver addressResolver) PoolManager(farm, addressResolver) public {
+        SETTINGS = settings;
     }
 
     /* ========== MUTATIVE FUNCTIONS ========== */
 
+    /**
+    * @dev Creates a new pool
+    * @param poolName Name of the pool
+    * @param performanceFee Performance fee for the pool
+    */
     function createPool(string memory poolName, uint performanceFee) external {
         require(bytes(poolName).length < 30, "Pool name must have less than 30 characters");
-        require(performanceFee <= Settings(getSettingsAddress()).getMaximumPerformanceFee(), "Cannot exceed maximum performance fee");
-        require(getUserPools(msg.sender).length < Settings(getSettingsAddress()).getMaximumNumberOfPoolsPerUser(), "Cannot exceed maximum number of pools per user");
+        require(performanceFee <= SETTINGS.getParameterValue("MaximumPerformanceFee"), "Cannot exceed maximum performance fee");
+        require(getUserPools(msg.sender).length < SETTINGS.getParameterValue("MaximumNumberOfPoolsPerUser"), "Cannot exceed maximum number of pools per user");
 
         _createPool(poolName, performanceFee, msg.sender);
     }
 
+    /**
+    * @dev Withdraw cUSD from the given pool
+    * @param poolAddress Address of the pool
+    * @param amount Amount of cUSD to withdraw from the pool
+    */
     function withdraw(address poolAddress, uint amount) external isValidPoolAddress(poolAddress) {
         require(amount > 0, "Withdrawal amount must be greater than 0");
 
