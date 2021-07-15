9ec22f111f5b0cde3cc4eb6ff2aa287b98715a9d
Xavier Negron <xaviernegron888@gmail.com>
2021-07-15 14:00:07 -0500

    Removed strategy related contracts and tests

--- old contracts/interfaces/IPoolRewardsEscrow.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.5.0;
-
-interface IPoolRewardsEscrow {
-
-    function claimPoolRewards(address user, uint amount) external;
-}
--- old contracts/interfaces/IStrategyVotingEscrow.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.5.0;
-
-interface IStrategyVotingEscrow {
-
-    function claimRewards(address user, uint amount) external;
-}
