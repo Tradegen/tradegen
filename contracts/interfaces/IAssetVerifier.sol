69526d33f3da82b505e03601c560e8ee328e794d
Xavier Negron <xaviernegron888@gmail.com>
2021-08-14 18:49:41 -0500

    Set up verifier contracts

---
+++ contracts/interfaces/IAssetVerifier.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0;
+pragma experimental ABIEncoderV2;
+
+interface IAssetVerifier {
+    struct MultiTransaction {
+        address to;
+        bytes txData;
+    }
+
+  function prepareWithdrawal(address pool, address asset, uint portion, address to) external view returns (address, uint, MultiTransaction[] memory transactions);
+
+  function getBalance(address pool, address asset) external view returns (uint balance);
+
+  function getDecimals(address asset) external view returns (uint decimals);
+}
---
+++ contracts/interfaces/IVerifier.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.5.0;
+
+interface IVerifier {
+    function verify(address to, bytes calldata data) external returns (bool);
+
+    event ExchangeFrom(address fundAddress, address sourceAsset, uint sourceAmount, address destinationAsset, uint timestamp);
+}
