1a7840666770e362ad936ae49cfdbc5409d9fc68
Xavier Negron <xaviernegron888@gmail.com>
2021-08-14 16:17:25 -0500

    Removed margin trading contracts

--- contracts/interfaces/IInterestRewardsPoolEscrow.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity >=0.5.0;
-
-interface IInterestRewardsPoolEscrow {
-    
-    function getCurrentRewardRate() external view returns (uint);
-
-    function claimRewards(address user, uint amount) external;
-}
