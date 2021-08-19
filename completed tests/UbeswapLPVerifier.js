6bda0b92e3247a2af2c73ffc47436251bcaec44e
Xavier Negron <xaviernegron888@gmail.com>
2021-08-19 18:13:15 -0500

    Test UbeswapLP verifier

--- completed tests/UbeswapLPVerifier.js
+++ completed tests/UbeswapLPVerifier.js
@@ -11,18 +11,21 @@ const getAccount3 = require('../get_account').getAccount3;
 const web3 = new Web3('https://alfajores-forno.celo-testnet.org');
 const kit = ContractKit.newKitFromWeb3(web3);
 
-const ERC20Verifier = require('../build/contracts/ERC20Verifier.json');
+const UbeswapLPVerifier = require('../build/contracts/UbeswapLPVerifier.json');
 const AddressResolver = require('../build/contracts/AddressResolver.json');
 const AssetHandler = require('../build/contracts/AssetHandler.json');
 const TradegenERC20 = require('../build/contracts/TradegenERC20.json');
 const BaseUbeswapAdapter = require('../build/contracts/BaseUbeswapAdapter.json');
 
-var contractAddress = "0x4c7bCEFD708Fbb169a447aD826F0836ba3362f6E";
+var contractAddress = "0xE717ce7fb6481Bb67c2C0f5701368ff6f111229b";
 var addressResolverAddress = "0x2BE72721aBe391Ff84E090dC4247AB873572c2A2";
 var assetHandlerAddress = "0x6969BEF2BC62864DbbeCf00C3d065670Cb355662";
 var baseUbeswapAdapterAddress = "0x7380D2C82c53271677f07C5710fEAb66615d1816";
 
 var TGEN = "0xb79d64d9Acc251b04A3Ca9f811EFf49Bde52BbbC";
+
+const farm_CELO_sCELO = "0xd4C9675b0AE1397fC5b2D3356736A02d86347f2d";
+var CELO_sCELO = "0xA31cE6Df9Bd21CFdD3CeF62E57f0AB3461Bc304D";
 
 var sCELO = "0xb9B532e99DfEeb0ffB4D3EDB499f09375CF9Bf07";
 var cUSD = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
@@ -33,31 +36,31 @@ var ubeswapRouterVerifierAddress = "0x7634459A3F9118b6492a7343B1Ec26f7a5729bAC";
 
 function initContract()
 { 
-    let instance = new web3.eth.Contract(ERC20Verifier.abi, contractAddress);
+    let instance = new web3.eth.Contract(UbeswapLPVerifier.abi, contractAddress);
     let addressResolverInstance = new web3.eth.Contract(AddressResolver.abi, addressResolverAddress);
     let assetHandlerInstance = new web3.eth.Contract(AssetHandler.abi, assetHandlerAddress);
     let tradegenInstance = new web3.eth.Contract(TradegenERC20.abi, TGEN);
     let baseUbeswapAdapterInstance = new web3.eth.Contract(BaseUbeswapAdapter.abi, baseUbeswapAdapterAddress);
-    
-    it('Initialize external contracts', async () => {
+
+    it('Initialize farms', async () => {
         let account = await getAccount();
         kit.connection.addAccount(account.privateKey);
         
-        //Add AssetHandler contract address to AddressResolver if needed
-        let txObject = await addressResolverInstance.methods.setContractAddress("AssetHandler", assetHandlerAddress);
+        //Initialize farms
+        let txObject = await instance.methods.initializeFarms();
         let tx = await kit.sendTransactionObject(txObject, { from: account.address }); 
         let receipt = await tx.waitReceipt();
 
-        let data = await addressResolverInstance.methods.getContractAddress("AssetHandler").call();
+        let result1 = receipt.events.InitializedFarms;
+        console.log(result1);
+    });
+    
+    it('Get farm', async () => {
+        let account = await getAccount();
+        kit.connection.addAccount(account.privateKey);
+        
+        let data = await instance.methods.ubeswapFarms(CELO_sCELO).call();
         console.log(data);
-
-        //Add UbeswapRouterVerifier as contract verifier
-        let txObject1 = await addressResolverInstance.methods.setContractVerifier(ubeswapRouterAddress, ubeswapRouterVerifierAddress);
-        let tx1 = await kit.sendTransactionObject(txObject1, { from: account.address }); 
-        let receipt1 = await tx1.waitReceipt()
-
-        let data1 = await addressResolverInstance.methods.contractVerifiers(ubeswapRouterAddress).call();
-        console.log(data1);
     });
     
     it('Verify with correct format and approved spender', async () => {
@@ -82,7 +85,7 @@ function initContract()
         let result1 = receipt.events.Approve;
         console.log(result1);
     });
-
+    
     it('Verify with correct format and unsupported spender', async () => {
         let account = await getAccount();
         kit.connection.addAccount(account.privateKey);
@@ -112,7 +115,7 @@ function initContract()
             console.log(err);
         }
     });
-
+    
     it('Verify with incorrect format', async () => {
         let account = await getAccount();
         kit.connection.addAccount(account.privateKey);
@@ -139,15 +142,15 @@ function initContract()
             'result1 should be undefined'
         );
     });
-
+    
     it('Get balance', async () => {
         let account = await getAccount();
         kit.connection.addAccount(account.privateKey);
 
-        let data = await instance.methods.getBalance(account.address, CELO).call();
+        let data = await instance.methods.getBalance(account.address, CELO_sCELO).call();
         console.log(data);
     });
-
+    
     it('Get decimals', async () => {
         let account = await getAccount();
         kit.connection.addAccount(account.privateKey);
