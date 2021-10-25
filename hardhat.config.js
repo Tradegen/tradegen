defafa673d5b7a9432e010439d93bd16a16888f9
Xavier Negron <xaviernegron888@gmail.com>
2021-10-25 08:01:24 -0500

    Update config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,95 +1,32 @@
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-solhint");
-require("@nomiclabs/hardhat-waffle");
-require("@ubeswap/hardhat-celo");
-const {
-  additionalOutputSelection,
-  fornoURLs,
-  ICeloNetwork,
-} = require("@ubeswap/hardhat-celo");
-require("dotenv/config");
-require("hardhat-abi-exporter");
-require("hardhat-gas-reporter");
-const { removeConsoleLog } = require("hardhat-preprocessor");
-require("hardhat-spdx-license-identifier");
-const { HardhatUserConfig, task } = require("hardhat/config");
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { toWei } = require('web3-utils');
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-truffle5')
 
-/*
-task("deploy", "Deploys a step", (async (...args) =>
-  (await import("./tasks/deploy")).deploy(...args)) as ActionType<{
-  step: string;
-}>).addParam("step", "The step to deploy");*/
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  abiExporter: {
-    path: "./build/abi",
-    //clear: true,
-    flat: true,
-    // only: [],
-    // except: []
-  },
-  defaultNetwork: "alfajores",
-  gasReporter: {
-    enabled: process.env.REPORT_GAS ? true : false,
-    currency: "USD"
-  },
-  networks: {
-    cmainnet: {
-      accounts: [process.env.PRIVATE_KEY1 ?? ''],
-      chainId: 42220,
-      gas: 6000000,
-      gasPrice: 0.5 * 10 ** 9,
-      url: 'https://forno.celo.org',
-      live: true
-    },
-    alfajores: {
-      accounts: [process.env.PRIVATE_KEY1 ?? '', process.env.PRIVATE_KEY2 ?? ''],
-      chainId: 44787,
-      gas: 6000000,
-      gasPrice: 0.5 * 10 ** 9,
-      url: 'https://alfajores-forno.celo-testnet.org',
-      live: true
-    },/*
-    hardhat: {
-      chainId: 31337,
-      accounts: [process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2],
-    },*/
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./build/cache",
-    artifacts: "./build/artifacts",
-  },
-  preprocess: {
-    eachLine: removeConsoleLog(
-      (bre) =>
-        bre.network.name !== "hardhat" && bre.network.name !== "localhost"
-    ),
-  },
   solidity: {
-    version: "0.8.3",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 999999,
       },
-      metadata: {
-        useLiteralContent: true,
-      },
-      outputSelection: additionalOutputSelection,
     },
   },
-  spdxLicenseIdentifier: {
-    overwrite: false,
-    runOnCompile: true,
-  },
-  namedAccounts: {
-    deployer: 0,
-  },
   mocha: {
-		timeout: 120e3, // 120s
-	},
-};
+    timeout: 1000000,
+  },
+}
